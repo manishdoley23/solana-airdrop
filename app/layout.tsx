a19d07ff09997aded8e67c608f2aadb9dfb4f13c
manishdoley23 <manishdoley23@gmail.com>
2024-04-15 11:26:42 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,22 +38,26 @@ export default function RootLayout({
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+				<Providers
+					themeProps={{ attribute: "class", defaultTheme: "dark" }}
+				>
 					<div className="relative flex flex-col h-screen">
-						<Navbar />
+						{/* <Navbar /> */}
 						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
 							{children}
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
-							<Link
+							{/* <Link
 								isExternal
 								className="flex items-center gap-1 text-current"
 								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
 								title="nextui.org homepage"
 							>
-								<span className="text-default-600">Powered by</span>
+								<span className="text-default-600">
+									Powered by
+								</span>
 								<p className="text-primary">NextUI</p>
-							</Link>
+							</Link> */}
 						</footer>
 					</div>
 				</Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,115 @@
+"use client";
+
 import { Link } from "@nextui-org/link";
+import { Code } from "@nextui-org/code";
 import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
+import { useState } from "react";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
+	const [airDropAddress, setAirDropAddress] = useState("");
+	const [solAmount, setSolAmount] = useState(0);
+	const [solNetwork, setSolNetwork] = useState("");
+
+	const getSolHandler = async () => {
+		if (solNetwork === "" || solAmount === 0 || airDropAddress === "") {
+			toast("Please fill all the fields");
+			return;
+		}
+
+		const connection = new Connection(solNetwork, "confirmed");
+		const address = new PublicKey(airDropAddress);
+		const signature = await connection.requestAirdrop(
+			address,
+			solAmount * LAMPORTS_PER_SOL
+		);
+		const { blockhash, lastValidBlockHeight } =
+			await connection.getLatestBlockhash();
+		const res = await connection.confirmTransaction({
+			blockhash,
+			lastValidBlockHeight,
+			signature,
+		});
+		console.log("res", res);
+	};
+
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
+		<section className="flex w-full justify-center items-center">
+			<ToastContainer
+				position="bottom-center"
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="dark"
+				transition={Bounce}
+			/>
+			<div className="flex flex-col">
+				<Input
+					key={"inside"}
+					type="text"
+					label="Enter solana account address"
+					labelPlacement={"inside"}
+					radius="sm"
+					onChange={(e) => {
+						setAirDropAddress(e.target.value);
+					}}
+				/>
+				<div className="w-full flex gap-10 items-center mt-10">
+					<Input
+						key={"inside"}
+						type="number"
+						label="Airdrop amount"
+						labelPlacement={"inside"}
+						radius="sm"
+						endContent={"SOL"}
+						onChange={(e) => setSolAmount(parseInt(e.target.value))}
+					/>
 
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
+					<div className="flex flex-col justify-center gap-5">
+						<Button
+							onClick={() =>
+								setSolNetwork("https://api.devnet.solana.com")
+							}
+							variant="bordered"
+							radius="sm"
+						>
+							DEVNET
+						</Button>
+						<Button
+							onClick={() =>
+								setSolNetwork("https://api.testnet.solana.com")
+							}
+							variant="bordered"
+							radius="sm"
+						>
+							TESTNET
+						</Button>
+					</div>
+				</div>
 
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
+				{solNetwork !== "" && (
+					<p className="bg-green-800 px-3 py-1 text-white rounded-md mt-5">
+						{solNetwork}
+					</p>
+				)}
+
+				<Button onClick={getSolHandler} className="mt-5" radius="sm">
+					GET SOL
+				</Button>
 			</div>
 		</section>
 	);
